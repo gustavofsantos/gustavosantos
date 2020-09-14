1defce5f02a2f7a04bf7fd3b0dac0ad27d4c608e
Gustavo Santos <gustavo@after.sale>
2020-09-14 20:10:18 -0300

    first version

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+var profile = {
+  name: "Gustavo Santos",
+  description: "functional programming adept",
+  email: "gustavofsantos@outlook.com",
+  website: "gustavosantos.dev",
+  github: "gustavofsantos",
+};
+
+console.info(profile);
